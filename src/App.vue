87920905f4a3a7a5a946a29e4aa996fe6a277944
Emestay <103575914+Emestay@users.noreply.github.com>
2023-03-15 17:48:39 +0100

    parfait

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,53 @@
 <template>
-  <div class="container min-h-screen flex flex-col items-center justify-center bg-gray-100">
-    <h1 class="text-4xl mb-4 font-semibold">SoWeSign en un clique</h1>
-    
-    </p>
-    <div class="w-full max-w-2xl p-4 bg-white shadow-md rounded flex">
-      <div class="w-1/2">
-        <canvas ref="canvas" width="400" height="400" @mousedown="startDrawing" @mousemove="draw" @mouseup="stopDrawing"
-          class="border-2 border-gray-300"></canvas>
-        <div class="controls mt-4 flex flex-wrap gap-2">
-          <button @click="undo" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Reset moi
+  <div class="min-h-screen bg-slate-100">
+    <div class="flex flex-col items-center justify-center h-full">
+      <br>
+      <h1 class="text-7xl mb-4 font-extrabold text-blue-500 to-blue-900">
+  SoWeSign en un clique
+</h1>
+<br>
+
+
+      <div class="w-full max-w-2xl p-4 bg-white shadow-lg rounded flex flex-col items-center">
+        <canvas ref="canvas" width="600" height="400" @mousedown="startDrawing" @mousemove="draw" @mouseup="stopDrawing"
+          class="border-2 border-gray-300 shadow-lg"></canvas>
+        <div class="mt-2 flex flex-wrap gap-2 items-center justify-center">
+          <button @click="undo" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded shadow-lg">Reset moi
             ça</button>
-          <button @click="clearCanvas" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Ctrl
+          <button @click="clearCanvas" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded shadow-lg">Ctrl
             Z</button>
           <button @click="generateBookmarkletCode"
-            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Générer le code
-            Bookmarklet</button>
+            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded shadow-lg">Générer le code</button>
           <button v-if="showBookmarkletCode" @click="copyCode"
-            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Ctrl C</button>
+            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded shadow-lg">Ctrl C</button>
         </div>
-      </div>
-      <div class="w-1/2" v-if="showBookmarkletCode">
-        <textarea ref="bookmarkletCode" readonly :value="minifiedCode" rows="10" cols="50"
-          class="mt-4 w-full p-2 border border-gray-300 rounded"></textarea>
-      </div>
-    </div>
-    <div class="p-4 bg-gray-100 rounded w-full max-w-2xl mt-4">
-      <h2 class="text-lg font-semibold mb-2">Qu'est-ce qu'un bookmarklet ?</h2>
-      <p class="text-gray-600 text-sm mb-4">Un bookmarklet est un petit programme JavaScript qui peut être enregistré
-        dans les favoris ou les signets de votre navigateur. Lorsqu'il est cliqué, il exécute le code JavaScript contenu
-        dans le bookmarklet sur la page web actuelle. Les bookmarklets peuvent être utilisés pour effectuer des tâches
-        spécifiques sur des pages web, comme ajouter un bouton de partage de médias sociaux, afficher des informations
-        supplémentaires sur une page ou même dessiner sur une page web, comme dans l'exemple de code Vue.js que vous
-        avez fourni.</p>
-      <p class="text-gray-600 text-sm">Le code JavaScript contenu dans un bookmarklet doit être court et efficace pour
-        ne pas ralentir la page web et pour s'assurer qu'il fonctionne sur toutes les pages web. C'est pourquoi il est
-        important de minifier le code avant de l'insérer dans un bookmarklet.</p>
-    </div>
 
+      </div>
+      <div class="w-1/2 flex justify-end" v-if="showBookmarkletCode">
+        <textarea ref="bookmarkletCode" readonly :value="minifiedCode" rows="10" cols="50"
+          class="mt-4 w-full p-2 border border-gray-300 rounded shadow-lg"></textarea>
+      </div>
+      <div class="p-4 bg-gray-100 rounded w-full max-w-2xl mt-4 shadow-lg">
+        <h2 class="text-lg font-semibold mb-2">Qu'est-ce qu'un bookmarklet ?</h2>
+        <p class="text-gray-600 text-sm mb-4">Un bookmarklet est un petit programme JavaScript qui peut être enregistré
+          dans les favoris ou les signets de votre navigateur. Lorsqu'il est cliqué, il exécute le code JavaScript contenu
+          dans le bookmarklet sur la page web actuelle. Les bookmarklets peuvent être utilisés pour effectuer des tâches
+          spécifiques sur des pages web, comme ajouter un bouton de partage de médias sociaux, afficher des informations
+          supplémentaires sur une page ou même dessiner sur une page web, comme dans l'exemple de code Vue.js que vous
+          avez fourni.</p>
+        <p class="text-gray-600 text-sm">Le code JavaScript contenu dans un bookmarklet doit être court et efficace pour
+          ne pas ralentir la page web et pour s'assurer qu'il fonctionne sur toutes les pages web. C'est pourquoi il est
+          important de minifier le code avant de l'insérer dans un bookmarklet.</p>
+      </div>
+      <br>
+      <br>
+    </div>
   </div>
-</div></template>
+
+</template>
+
+
+
 
 
 
