280363c435946237f51318577695cc9174cd2453
Emestay <103575914+Emestay@users.noreply.github.com>
2023-03-15 15:48:27 +0100

    oui

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,8 @@
   <div class="container min-h-screen flex flex-col items-center justify-center bg-gray-100">
     <h1 class="text-4xl mb-4 font-semibold">Application de dessin Vue.js</h1>
     <p class="mb-4 text-xl text-gray-600 max-w-3xl text-center">
-      Une simple application de dessin créée avec Vue.js et stylisée avec Tailwind CSS. Le site est déployé à cette
-      adresse : <a href="https://wonderful-puppy-a2028c.netlify.app/" class="text-blue-500"
-        target="_blank">https://wonderful-puppy-a2028c.netlify.app/</a>
+      Une simple application de dessin créée avec Vue.js et stylisée avec Tailwind CSS. Un bookmarklet c'est quoi? <a href="https://www.debugbar.com/fr/bookmarklets-javascript/" class="text-blue-500"
+        target="_blank">https://www.debugbar.com/fr/bookmarklets-javascript/</a>
     </p>
     <div class="w-full max-w-2xl p-4 bg-white shadow-md rounded">
       <canvas ref="canvas" width="400" height="400" @mousedown="startDrawing" @mousemove="draw" @mouseup="stopDrawing"
@@ -22,16 +21,6 @@
       </div>
       <textarea v-if="showBookmarkletCode" ref="bookmarkletCode" readonly :value="minifiedCode" rows="10" cols="50"
         class="mt-4 w-full p-2 border border-gray-300 rounded"></textarea>
-    </div>
-    <div class="explanation mt-8 text-gray-600 max-w-3xl text-center">
-      <p class="mb-4">Un bookmarklet est un petit programme JavaScript stocké sous forme de favori dans un navigateur Web.
-        Il peut être utilisé pour exécuter des actions ou des modifications sur une page Web ou pour extraire des
-        informations de celle-ci.</p>
-      <p class="font-semibold mb-2">Ensuite :</p>
-      <ul class="mt-2 list-disc list-inside">
-        <li class="mb-1">minify js du code de sortie</li>
-        <li class="mb-1">faire un favoris en remplacant l'url par le code minifier</li>
-      </ul>
     </div>
 
   </div>
