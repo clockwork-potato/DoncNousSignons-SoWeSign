16e87ddcb1a68188f62169c5e3a432c8115b31a7
Emestay <103575914+Emestay@users.noreply.github.com>
2023-03-16 09:09:34 +0100

    bookmark

--- src/App.vue
+++ src/App.vue
@@ -22,14 +22,14 @@
             class="bg-yellow-500 hover:bg-yellow-800 text-white font-bold py-2 px-4 rounded shadow-lg">Ctrl C</button>
         </div>
       </div>
-      
+
 
       <div class="w-1/2 flex justify-end" v-if="showBookmarkletCode">
         <textarea ref="bookmarkletCode" readonly :value="minifiedCode" rows="10" cols="50"
           class="mt-4 w-full p-2 border border-gray-300 rounded shadow-lg"></textarea>
       </div>
       <br>
-      <a v-if="showBookmarkletCode" :href="minifiedCode"
+      <a v-if="showBookmarkletCode" :href="minifiedCode" 
         class="inline-flex items-center px-4 py-2 bg-blue-500 hover:bg-blue-700 text-white font-semibold rounded-lg shadow-md">
         <svg class="h-6 w-6 mr-2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
           <path
@@ -79,6 +79,13 @@ export default {
     this.context = this.$refs.canvas.getContext("2d");
   },
   methods: {
+    addToBookmarks() {
+      if (window.sidebar && window.sidebar.addPanel) { // Vérifie si la fonction addPanel est prise en charge
+        window.sidebar.addPanel(document.title, this.minifiedCode, ''); // Ajoute la page actuelle aux favoris
+      } else { 
+        alert("Impossible d'ajouter aux favoris automatiquement. Veuillez ajouter manuellement cette page à vos favoris.");
+      }
+    },
     getCursorPosition(event) {
       const rect = this.$refs.canvas.getBoundingClientRect();
       return {
@@ -176,4 +183,5 @@ hr {
   background-color: #333;
   margin-top: 1rem;
   margin-bottom: 1rem;
-}</style>
+}
+</style>
